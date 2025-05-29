8eeb22cc83b444fc98e6ad5b505c7b02f6a2c964
Jan Katzer <jan@katzer.dev>
2025-05-30 00:40:13 +0200

    Fix sw downloads immediately failing in chrome

--- sw.js
+++ sw.js
@@ -16,53 +16,63 @@ self.addEventListener('message', e => {
 	if (e.data.msg === "start-dl") {
 		const fileIdx = e.data.fileIdx;
 		const fileByteSize = e.data.fileByteSize;
+		const encodedFilename = e.data.encodedFilename;
 		
 		const port = e.ports[0];
 		
-		const stream = new ReadableStream({
-			start(controller) {
-				port.onmessage = ({data}) => {
-					// DEBUG:
-					console.log(`SW: got msg: ${JSON.stringify(data)} (${data.chunk.byteLength} bytes)`)
-					
-					if (data.error) {
-						controller.error(data.error);
-					} else {
-						const chunk = new Uint8Array(data.chunk);
-						controller.enqueue(chunk);
-						
-						if (data.done) {
-							console.log("DL DONE, calling ReadableStream.close()")
-							controller.close();
-						}
-					}
-				};
-			},
-			cancel(reason) {
-				// DEBUG:
-				console.log(`ReadableStream got canceled: ${reason}`)
-				
-				// Call back to main context to
-				// notify about canceled download
-				// so we can notify to sender to
-				// stop sending bytes
-				port.postMessage({
-					"canceled": true,
-				})
-			},
+		const stream = e.data.mainThreadStream;
+//		const stream = new ReadableStream({
+//			start(controller) {
+//				port.onmessage = ({data}) => {
+//					// DEBUG:
+//					console.log(`SW: got msg: ${JSON.stringify(data)} (${data.chunk.byteLength} bytes)`)
+//
+//					if (data.error) {
+//						controller.error(data.error);
+//					} else {
+//						const chunk = new Uint8Array(data.chunk);
+//						controller.enqueue(chunk);
+//
+//						if (data.done) {
+//							console.log("DL DONE, calling ReadableStream.close()")
+//							controller.close();
+//						}
+//					}
+//				};
+//			},
+//			cancel(reason) {
+//				// DEBUG:
+//				console.log(`ReadableStream got canceled: ${reason}`)
+//
+//				// Call back to main context to
+//				// notify about canceled download
+//				// so we can notify to sender to
+//				// stop sending bytes
+//				port.postMessage({
+//					"canceled": true,
+//				})
+//			},
+//		});
+		
+		const headers = {
+			'Content-Type': 'application/octet-stream',
+			'Content-Disposition': `attachment; filename*=UTF-8''${encodedFilename}`,
+			'Content-Length': String(fileByteSize),
+		}
+		const response = new Response(stream, {
+			status: 200,
+			statusText: "OK",
+			headers
 		});
 		
-		const response = new Response(stream, {
-			headers: {
-				'Content-Type': 'application/octet-stream',
-				'Content-Disposition': 'attachment',
-				'Content-Length': String(fileByteSize),
-			}
-		});
+		// DEBUG:
+		console.log("SW: response headers:", headers)
 		
 		// Add to dir of active downloads
 //		self.activeDownloadStreamsByFileIdx[fileIdx] = response;
 		self.lastDownloadStream = response;
+		
+		port.postMessage({ "isReady": true })
 	}
 });
 
